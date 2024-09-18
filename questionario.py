b83e35c4bccb503d96b9202fdee9964b0cc6f57c
Davi <zanellamafradavilucas@gmail.com>
2024-09-18 14:23:49 -0300

    primeiro commit

---
+++ questionario.py
@@ -0,0 +1,76 @@
+print ('bem vindos ao questionario sobre redes,escolha uma alternativa veja se esta correta ou errada')
+
+print('vamos começar')
+
+print('Qual é o conector que é utilizado para fazer o cabo de rede?')
+print('A RJ-45')
+print('B RG-XJ')
+print('C RG-45')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a':
+    print ('resposta certa')
+if resposta == 'b':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+    
+print('Qual é a sequencia de cores do cabo de rede?')
+print('A Branco do verde, verde, branco do azul, azul, branco do laranja, laranja, branco do marrom, marrom')
+print('B Branco do verde, verde, branco do azul, laranja, branco do laranja, azul, branco do marrom, marrom')
+print('C Branco do verde, azul, branco do azul, verde, branco do marrom, laranja, branco do laranja, marrom')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'b':
+    print ('resposta certa')
+if resposta == 'c':
+    print ('resposta errada')
+    
+print('Qual o nome do alicate que é utilizado para crimpar o conector do cabo de rede?')
+print('A Alicate de grimpar ')
+print('b Alicate de compactar  ')
+print('c Alicate de crimpar ')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'b':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta certa')
+    
+print('Até quantos metres pode chegar um cabo de rede?')
+print('A 100m')
+print('B 200m')
+print('C 500m')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a':
+    print ('resposta certa')
+if resposta == 'b':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+    
+print('O que é um cabo de rede?')
+print('A É um padrão que interliga dispositivos por meio de um cabo de rede com fios de par traçando e conectores do tipo RJ-45')
+print('B É um cabo que transmite vídeos.')
+print('C É um cabo de rede que conecta dispositivos periféricos (Mouse teclado)')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a':
+    print ('resposta certa')
+if resposta == 'b':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+    
+
+    
